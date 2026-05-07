7477461645e30dd351bf85897202e3d7ed452463
LennyRocha <20233tn094@utez.edu.mx>
2026-05-06 22:04:40 -0600

    [UPDATE] Se cambió de commonJs a module [FIX] Conexión a la base de datos correcta.

--- backend/config/dbconfig.js
+++ backend/config/dbconfig.js
@@ -1,8 +1,10 @@
-const sequelize = require('sequelize');
-
-const db = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config();
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
+    dialect: 'mysql',
 });
-module.exports = db;
+
+export default db;
--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const { sequelize } = require('./config/dbconfig');
+import e from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import db from './config/dbconfig.js'
 
-require('dotenv').config();
-
-const app = express();
+const app = e();
 
 app.use(cors());
-app.use(express.json());
+app.use(e.json());
 
 app.use(morgan('dev'));
 
 app.listen(process.env.PORT, async () => {
     console.log(`Server is running on port ${process.env.PORT}`);
     try {
-        await sequelize.authenticate();
+        await db.authenticate();
         console.log('Database connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
